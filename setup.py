d1639ac1189d00fab6a51fc4a827ec834889fd4f
AngusP <a7cp3ar50n@gmail.com>
2017-06-30 10:06:05 +0100

    Update setup.py with 3.6

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
         # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
-        'Development Status :: 3 - Alpha',
-        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         #'Environment :: Web Environment',
         #'Operating System :: POSIX',
         #'License :: OSI Approved :: MIT License',
