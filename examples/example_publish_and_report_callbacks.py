4b1f532dd2c478de9ff8e1f9a8fae318242e405a
Branislav Pilnan <brano@robotical.io>
2022-01-11 12:38:16 +0100

    Keep default logging config & follow naming conventions
    This makes it easier to see, what messages will people get by default.
    This is especially interesting if one of the callbacks throws an
    exception.

--- examples/example_publish_and_report_callbacks.py
+++ examples/example_publish_and_report_callbacks.py
@@ -12,12 +12,12 @@ import logging
 import time
 
 # Setup Logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
 
 # Callback function for when a publish message is received
 # Example is new servo positions, accelerometer data, etc.
-def publishCallback(topic):
+def publish_callback(topic):
     topic_name = (
         "SERVOS" if topic == Marty.PUBLISH_TOPIC_SERVOS else
         "ACCELEROMETER" if topic == Marty.PUBLISH_TOPIC_ACCELEROMETER else
@@ -26,12 +26,12 @@ def publishCallback(topic):
         "ROBOT_STATUS" if topic == Marty.PUBLISH_TOPIC_ROBOT_STATUS else
         "UNKNOWN_TOPIC"
     )
-    logger.debug(f"publishCallback {topic} ({topic_name})")
+    print(f"publish_callback {topic} ({topic_name})")
 
 # Callback function for when a report message is received
 # Example is free-fall or over-current detected
-def reportCallback(report_info):
-    logger.debug(f"reportCallback {report_info}")
+def report_callback(report_info):
+    print(f"report_callback {report_info}")
 
 # Start Marty
 # This code can be blocking because we're going to be getting messages
@@ -41,8 +41,8 @@ def reportCallback(report_info):
 my_marty = Marty("wifi", "192.168.0.42", blocking=True, subscribeRateHz=1)
 
 # Register callbacks
-my_marty.register_publish_callback(publishCallback)
-my_marty.register_report_callback(reportCallback)
+my_marty.register_publish_callback(publish_callback)
+my_marty.register_report_callback(report_callback)
 
 # Move Marty's eyes
 my_marty.eyes(100)
