d81c779ba368461904ab3c8ccd32233699edde28
nikthegeek1 <ntheodoropoulos@outlook.com>
2023-04-24 20:17:40 +0300

    added docgen shell script

--- docgen.sh
+++ docgen.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 pip install -r dev-requirements.txt
 pydoc-markdown
 python -c "from docgen.ReformatDocs import reformatDocs as rD; rD()"
-clip < "%~dp0\build\docs\content\docs\api-documentation-edited.md"
+pbcopy < "$(dirname $0)/build/docs/content/docs/api-documentation-edited.md"
